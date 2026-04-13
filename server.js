11ef57cd673d36fe2bfef45625e7d2fb2179cd5a
TutionPao Admin <tutionpao@example.com>
2026-04-14 01:15:06 +0530

    Update default port to 80

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', (req, res) => {
     res.send("TutionPao Real API is Running! 🚀🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
